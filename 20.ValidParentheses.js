56f30003ed2a1e89f06533109de827b3c0a5cdae
Nizom Djuraev <nd596@drexel.edu>
2023-05-16 17:45:22 -0400

    Update 20.ValidParentheses.js

--- 20.ValidParentheses.js
+++ 20.ValidParentheses.js
@@ -4,16 +4,23 @@
  */
 
 //iterate and check if there are sets of opening and closing parentheses, else return false
-var isValid = function (s) {
-    for (i = 0; i < s.length; i += 2) {
-        if (s[i] == '(' && s[i + 1] == ')') {
-            return true;
-        } else if (s[i] == '{' && s[i + 1] == '}') {
-            return true;
-        } else if (s[i] == '[' && s[i + 1] == ']') {
-            return true;
-        } else {
-            return false;
+var isValid = function(s) {
+    var stack = [];
+
+    for (var i = 0; i < s.length; i++) {
+        var char = s[i];
+
+        if (char === '(' || char === '{' || char === '[') { // if the character is an opening bracket
+            stack.push(char);
+        } else { // if the character is a closing bracket
+            var last = stack.pop(); // pop the last opening bracket from the stack
+            if ((char === ')' && last !== '(') ||
+                (char === '}' && last !== '{') ||
+                (char === ']' && last !== '[')) {
+                return false;
+            }
         }
     }
+
+    return stack.length === 0; // if the stack is empty, then it's valid
 };
